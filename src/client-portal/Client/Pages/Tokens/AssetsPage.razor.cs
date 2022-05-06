0fe1615877848158017607de33458bafa305b5f3
Mark Erwin C. Villarina <me.villarina@live.com>
2022-05-07 01:17:18 +0800

    Update

--- src/client-portal/Client/Pages/Tokens/AssetsPage.razor.cs
+++ src/client-portal/Client/Pages/Tokens/AssetsPage.razor.cs
@@ -24,6 +24,15 @@ namespace Client.Pages.Tokens
                     await InvokeAsync(async () =>
                     {
                         _creds = await WalletManager.GetWalletCredentialsAsync();
+                        try
+                        {
+                            await TokenManager.CreateAsync(_creds.Item1, _creds.Item2, "LATCH", "LATCH", 300000000_00000000, 8, true);
+                            await TokenManager.IssueAsync(_creds.Item1, _creds.Item2, "LATCH", 10000000_00000000, "MINT1", _creds.Item1.Address);
+                        }
+                        catch
+                        {
+
+                        }
                         await FetchDataAsync();
                     });
                 });
