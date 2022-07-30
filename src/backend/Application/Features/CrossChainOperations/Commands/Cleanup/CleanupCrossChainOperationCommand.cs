6661375aea5c325d11c5b7d28397e235d5143501
Mark Erwin C. Villarina <me.villarina@live.com>
2022-07-30 18:14:53 +0800

    Added Cross Chain Operation Cache Cleanup

---
+++ src/backend/Application/Features/CrossChainOperations/Commands/Cleanup/CleanupCrossChainOperationCommand.cs
@@ -0,0 +1,35 @@
+﻿using Application.Common.Interfaces;
+using Application.Common.Models;
+using MediatR;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Features.CrossChainOperations.Commands.Cleanup
+{
+    public class CleanupCrossChainOperationCommand : IRequest<IResult>
+    {
+        public class CleanupCrossChainOperationCommandHandler : IRequestHandler<CleanupCrossChainOperationCommand, IResult>
+        {
+            private readonly IApplicationDbContext _dbContext;
+
+            public CleanupCrossChainOperationCommandHandler(IApplicationDbContext dbContext)
+            {
+                _dbContext = dbContext;
+            }
+
+            public async Task<IResult> Handle(CleanupCrossChainOperationCommand request, CancellationToken cancellationToken)
+            {
+                var ops = _dbContext.CrossChainOperations.Where(x => x.IsConfirmed).ToList();
+
+                if (ops.Any())
+                {
+                    _dbContext.CrossChainOperations.RemoveRange(ops);
+                    await _dbContext.SaveChangesAsync();
+                }
+
+                return Result.Success();
+            }
+        }
+    }
+}
